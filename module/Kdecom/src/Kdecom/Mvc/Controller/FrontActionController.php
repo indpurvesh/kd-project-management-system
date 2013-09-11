ee7c84bb0dccd6d7c522c0f5d9e578def4c4105a
Purvesh <ind.purvesh@gmail.com>
2013-09-10 20:09:03 -0700

    Contact -- Started

--- module/Kdecom/src/Kdecom/Mvc/Controller/FrontActionController.php
+++ module/Kdecom/src/Kdecom/Mvc/Controller/FrontActionController.php
@@ -3,11 +3,14 @@
 namespace Kdecom\Mvc\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Authentication\Storage\Session;
 
 /**
  * Basic action controller
  */
 class FrontActionController extends AbstractActionController {
+
+    public $_paginationFilter;
 
     public function isUserLoggedIn() {
         $authService = $this->serviceLocator->get('auth_service');
@@ -15,6 +18,38 @@ class FrontActionController extends AbstractActionController {
             return true;
         }
         $this->redirect()->toRoute('login');
+    }
+
+    /*
+     * 
+     * @todo $gridKey and make it work automatically.
+     */
+
+    public function setUpPaginationFilter($paginationKey, $gridKeys) {
+        $this->_paginationFilter = new Session('pagination_storage');
+
+        $filterData = $this->_paginationFilter->read();
+
+        if (isset($filterData[$paginationKey])) {
+
+            foreach ($gridKeys as $key => $gridKey) {
+                if (isset($filterData[$paginationKey][$gridKey . "-order"])) {
+                    if ($filterData[$paginationKey][$gridKey . "-order"] == "ASC") {
+                        $filterData[$paginationKey][$gridKey . "-order"] = "DESC";
+                    } else {
+                        $filterData[$paginationKey][$gridKey . "-order"] = "ASC";
+                    }
+                } else {
+                    $filterData[$paginationKey][$gridKey . "-order"] = "ASC";
+                }
+            }
+        } else {
+            foreach ($gridKeys as $key => $gridKey) {
+                $filterData[$paginationKey][$gridKey . "-order"] = "ASC";
+            }
+        }
+        
+        $this->_paginationFilter->write($filterData);
     }
 
 }
